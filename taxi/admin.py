baa226166041aaeebe31e22771d723cbfe9545f9
Olha Rozdorozhna <olga.rozdorozhna@gmail.com>
2024-05-14 15:32:59 +0300

    Merge remote-tracking branch 'origin/develop' into develop

--- taxi/admin.py
+++ taxi/admin.py
@@ -1,15 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
 from .models import Driver, Car, Manufacturer
 
 
 @admin.register(Driver)
-class DriverAdmin(admin.ModelAdmin):
-    fieldsets = (
-        (None, {"fields": ("username", "password")}),
-        ("Personal info", {"fields": ("first_name", "last_name", "email")}),
-        ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser")}),
-        ("Additional info", {"fields": ("license_number",)}),
+class DriverAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('license_number',)}),
     )
+    list_display = UserAdmin.list_display + ('license_number',)
+    list_filter = UserAdmin.list_filter + ('license_number',)
+    search_fields = UserAdmin.search_fields + ('license_number',)
     add_fieldsets = (
         (
             None,
@@ -21,14 +23,14 @@ class DriverAdmin(admin.ModelAdmin):
         ("Personal info", {"fields": ("first_name", "last_name", "email")}),
         ("Additional info", {"fields": ("license_number",)}),
     )
-    list_display = ("username", "email", "license_number")
-    search_fields = ("username", "email", "license_number")
+
 
 
 @admin.register(Car)
 class CarAdmin(admin.ModelAdmin):
     list_display = ("model", "manufacturer")
     list_filter = ("manufacturer", "model")
+    search_fields = ("model",)
 
 
 @admin.register(Manufacturer)
